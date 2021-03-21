09eef1ce7a044b60c78d2dd8dd35d50f0be223aa
wladyslaw <wladyslawsahaidak@gmail.com>
2021-03-21 19:17:02 +0100

    na_odpierdol

--- src/store/reducers/covidDataReducer.ts
+++ src/store/reducers/covidDataReducer.ts
@@ -5,7 +5,7 @@ import {
 } from "../../types/covidData";
 
 const initialState: CovidDataState = {
-  covidData: [],
+  covidData: {},
   loading: false,
   error: null,
 };
@@ -16,11 +16,11 @@ export const covidDataReducer = (
 ): CovidDataState => {
   switch (action.type) {
     case CovidDataTypes.FETCH_COVID_DATA:
-      return { loading: true, error: null, covidData: [] };
+      return { loading: true, error: null, covidData: {} };
     case CovidDataTypes.FETCH_COVID_DATA_SUCCESS:
       return { loading: false, error: null, covidData: action.payload };
     case CovidDataTypes.FETCH_COVID_DATA_ERROR:
-      return { loading: false, error: action.payload, covidData: [] };
+      return { loading: false, error: action.payload, covidData: {} };
     default:
       return state;
   }
